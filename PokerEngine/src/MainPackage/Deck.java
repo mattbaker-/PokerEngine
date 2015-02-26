abe8b9780711a83adb3abcf6ae9d74979a188a0b
mattbaker- <mbaker615@me.com>
2015-02-26 09:52:31 -0500

    new commit

--- PokerEngine/src/MainPackage/Deck.java
+++ PokerEngine/src/MainPackage/Deck.java
@@ -1,44 +1,56 @@
 package MainPackage;
 
 public class Deck {
-	
-	private Card[] Deck;
-	
+
+	// private Card[] DeckOfCards;
+	private int[] deck;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	
-public Deck() {
-		
-		Deck = new Card[52];
-		
-		for (int i=0; i < 13; i++) {
-			Deck[i].setSuit("Spades");
+
+	public Deck() {
+		deck = new int[52];
+		String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+		String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+				"Jack", "Queen", "King" };
+
+		// Initialize cards
+		for (int i = 0; i < deck.length; i++) {
+			deck[i] = i;
 		}
 		
-		for (int i=13; i < 25; i++) {
-			//
-		}
+		shuffle();
 		
-//		deck.shuffle();
-	}
-	
-//	void shuffle() {
-//		// shuffle deck of cards
-//	}
-//	
-//	Card draw() {
-//		// return next card
-//	}
-//	
-//	int cardsRemaining() {
-//		// how many cards remaining to be drawn
-//	}
-	
-	public void listCards() {
-		for (int i=0; i < Deck.length; i++) {
-			//
+		// Display the first four cards
+		for (int i = 0; i < deck.length; i++) {
+			String suit = suits[deck[i] / 13];
+			String rank = ranks[deck[i] % 13];
+			System.out.println(i +1 + ". Card number " + deck[i] + ": " + rank + " of "
+					+ suit);
 		}
 	}
+
+	void shuffle() {
+		// Shuffle the cards
+		for (int i = 0; i < deck.length; i++) {
+			// Generate an index randomly
+			int index = (int) (Math.random() * deck.length);
+			int temp = deck[i];
+			deck[i] = deck[index];
+			deck[index] = temp;
+		}
+	}
+
+	// Card draw() {
+	// // return next card
+	//
+	// }
+	
+	// int cardsRemaining() {
+	// // how many cards remaining to be drawn
+	//
+	// }
+
 }
